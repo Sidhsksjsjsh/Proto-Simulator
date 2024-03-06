9f95bf657e6c8d35f916135607fba9f1050f7f1b
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-03-06 19:16:55 +0900

    Update .lua

--- .lua
+++ .lua
@@ -4,18 +4,29 @@ local T1 = wndw:Tab("Main")
 local T2 = wndw:Tab("Hatch")
 
 local eggh = {}
+local W = {}
 local user = game.Players.LocalPlayer
 local workspace = game:GetService("Workspace")
 
-local args = {
-    [1] = workspace["World0"]["Eggs"]["Level0"]
-}
+--local args = {
+--    [1] = workspace["World0"]["Eggs"]["Level0"]
+--}
 
-game:GetService("ReplicatedStorage")["HatchEgg"]:FireServer(unpack(args))
+--game:GetService("ReplicatedStorage")["HatchEgg"]:FireServer(unpack(args))
+--table.insert(tbl,v.Name)
+for i,v in pairs(workspace:GetChildren()) do
+  if v.Name:find("World") then
+    for index,variable in pairs(v["Eggs"]:GetChildren()) do
+        if index == 1 or index == "1" then
+            table.insert(eggh,variable.Name)
+        end
+    end
+  end
+end
 
 for i,v in pairs(workspace:GetChildren()) do
   if v.Name:find("World") then
-    
+    table.insert(W,v.Name)
   end
 end
 
@@ -35,6 +46,18 @@ T1:Toggle("Auto spin",false,function(value)
     end
 end)
 
-T1:Dropdown("Select Egg level",eggh,function(value)
-    print(value)
+T2:Dropdown("Select World ( world1 = Level1 )",W,function(value)
+    _G.WLvl = value
+end)
+
+T2:Dropdown("Select Egg level",eggh,function(value)
+    _G.egglvl = value
+end)
+
+T2:Toggle("Auto hatch",false,function(value)
+    _G.hdc = value
+    while wait() do
+      if _G.hdc == false then break end
+      game:GetService("ReplicatedStorage")["HatchEgg"]:FireServer(workspace[_G.WLvl]["Eggs"][_G.egglvl])
+    end
 end)
